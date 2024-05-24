fa184bbbf19fdd09cce662bf3c19d6bb48a77587
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-05-24 09:36:12 +0545

    feat: form design

--- src/components/noteItem.jsx
+++ src/components/noteItem.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const NoteItem = (props) => {
   return (
-    <li className="py-6 px-4 cursor-pointer font-primary">
-      <h1>{props.title}</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur</p>
+    <li
+      className={`py-3 my-3 mx-4 rounded-lg cursor-pointer font-roboto ${
+        props.active && "bg-slate-700"
+      }`}
+    >
+      <div className={`px-2 ${props.active && "text-white"}`}>
+        <h1>{props.title}</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur</p>
+      </div>
     </li>
   );
 };
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import NoteItem from "./noteItem";
 
+import { CgNotes } from "react-icons/cg";
+
 const Sidebar = () => {
   return (
-    <div>
-      <h1 className="text-3xl px-4">Notes</h1>
+    <div className="py-6">
+      <div className="flex items-center px-4">
+        <CgNotes className="h-7 w-7 mr-2" />
+        <h1 className="text-3xl">Notes</h1>
+      </div>
 
       <div className="px-4 mt-10">
-        <input type="text" placeholder="Search" className="w-full py-4 px-4 outline-none border-none rounded-lg" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full py-4 px-4 outline-none border-none rounded-lg"
+        />
       </div>
 
       <ul>
         <NoteItem title="Kripa" />
-        <NoteItem title="Dipin" />
+        <NoteItem title="Dipin" active={true}  />
         <NoteItem title="David" />
         <NoteItem title="Prabin" />
         <NoteItem title="Pawan" />
